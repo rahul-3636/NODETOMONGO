3b269c12d3385d6f1f1e5d8bfa202cc9a1fc3822
rahul-3636 <rxhul36@gmail.com>
2025-08-06 15:51:28 +0530

    Add files via upload

---
+++ studentInfo.jsx
@@ -0,0 +1,31 @@
+import Student from "./studentmng";
+
+export default function Sinfo() {
+  return (
+    <>
+    <Student />
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>number</th>
+        </tr>
+        <tr>
+            <td>aaa</td>
+            <td>a@a.com</td>
+            <td>12345</td>
+        </tr>
+        <tr>
+            <td>bbb</td>
+            <td>b@b.com</td>
+            <td>67890</td>
+        </tr>
+        <tr>
+            <td>ccc</td>
+            <td>c@c.com</td>
+            <td>13579</td>
+        </tr>
+    </table>
+</>
+  );
+}
